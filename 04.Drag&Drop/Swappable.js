eb6b6f4bed4d23ba9aab2a682b5a335adff15679
NamgungJongMin <jmnamgung@gmail.com>
2022-12-02 11:15:49 +0900

    refactor: 04. Drag&Drop
    - 각 함수의 사용 이유 및 동작 설명 주석 추가
    - preventDefault enter에도 추가
    - languages swappable 밖으로 분리

--- 04.Drag&Drop/Swappable.js
+++ 04.Drag&Drop/Swappable.js
@@ -1,13 +1,15 @@
 // https://githut.info
 const languages = ['JavaScript', 'Java', 'Python', 'CSS', 'PHP', 'Ruby', 'C++', 'C', 'Shell', 'C#'];
-const suffleLanguages = [];
 
-const render = $swappable => {
-  // prettier-ignore
-  $swappable.innerHTML = `
+export default $swappable => {
+  const suffledLanguages = [];
+
+  const render = () => {
+    // prettier-ignore
+    $swappable.innerHTML = `
       <ul class="draggable-list">
-        ${suffleLanguages.map((language, index) =>
-          `<li class="${language === languages[index] ? 'right' : 'wrong'}">
+        ${suffledLanguages.map((language, index) =>`
+          <li class="${language === languages[index] ? 'right' : 'wrong'}">
             <div class="seq">${index + 1}</div>
             <div class="draggable" draggable="true">
               <p class="language-name">${language}</p>
@@ -16,27 +18,45 @@ const render = $swappable => {
           </li>`
           ).join('')}
       </ul>`;
-};
+  };
 
-const creatSuffleLanguages = () => {
-  const randomArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sort(() => Math.random() - 0.5);
+  // languages.forEach(item => {
+  //   const randomIndex = randomArray.splice(Math.floor(Math.random() * randomArray.length), 1);
+  //   suffleLanguages[randomIndex] = item;
+  // });
 
-  languages.forEach((item, index) => {
-    suffleLanguages[randomArray[index]] = item;
-  });
-};
+  // randomNumberArray 변수에 인덱스를 무작위 순서로 채워넣은 배열을 할당.
+  // 무작위 순서의 인덱스를 바탕으로 기존 languages의 해당 언어를 순서대로 suffledLanguages에 할당.
+  const creatSuffleLanguages = () => {
+    const randomNumberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].sort(() => Math.random() - 0.5);
 
-const changeLanguagePosition = (dragLanguage, dropLanguage) => {
-  const dragIndex = suffleLanguages.indexOf(dragLanguage);
-  const dropIndex = suffleLanguages.indexOf(dropLanguage);
+    languages.forEach((language, index) => {
+      suffledLanguages[randomNumberArray[index]] = language;
+    });
+  };
 
-  [suffleLanguages[dragIndex], suffleLanguages[dropIndex]] = [suffleLanguages[dropIndex], suffleLanguages[dragIndex]];
-};
+  const changeLanguagePosition = (dragLanguage, dropLanguage) => {
+    const dragIndex = suffledLanguages.indexOf(dragLanguage);
+    const dropIndex = suffledLanguages.indexOf(dropLanguage);
 
-export default $swappable => {
+    // prettier-ignore
+    [suffledLanguages[dragIndex], suffledLanguages[dropIndex]] =
+    [suffledLanguages[dropIndex], suffledLanguages[dragIndex]];
+  };
+
   creatSuffleLanguages();
-  render($swappable);
+  render();
 
+  /* ------------------------------ Event Handler ----------------------------- */
+  // drag and drop Web API 학습
+  // drop 이벤트가 발생하지 않는 문제 https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Drag_operations#drop
+
+  // -> 기본적으로 브라우저는 드래그한 대상이 드랍이 되지 않도록 설계 되어있음. 즉 브라우저가 기본 동작이 drop 이벤트 자체를 막고 있기 때문에 이벤트가 발생하지 않는다.
+  // 브라우저의 기본 동작을 제어함으로써 drop 이벤트 발생을 읽을 수 있음.
+  // dragover, dragenter에서 preventDefault를 해주었다.
+
+  // dataTransfer 프로퍼티를 이용하여 start 이벤트의 타겟 정보를 drop 이벤트에서도 접근할 수 있도록 setData 메서드를 사용하여 drag 이벤트 객체에 저장.
+  // $swappable의 자식요소가 가진 textContent를 읽어오는 것이 목적. 스페이싱도 text로 인식하기 때문에 trim()
   $swappable.addEventListener('dragstart', e => {
     e.dataTransfer.setData('dragLanguage', e.target.textContent.trim());
   });
@@ -47,6 +67,7 @@ export default $swappable => {
 
   $swappable.addEventListener('dragenter', e => {
     if (!e.target.matches('.draggable')) return;
+    e.preventDefault();
 
     e.target.parentNode.classList.add('over');
   });
@@ -63,6 +84,6 @@ export default $swappable => {
     e.target.parentNode.classList.remove('over');
 
     changeLanguagePosition(e.dataTransfer.getData('dragLanguage'), e.target.textContent.trim());
-    render($swappable);
+    render();
   });
 };
