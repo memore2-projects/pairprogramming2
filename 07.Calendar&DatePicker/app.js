001465f66b0f5020dc304f07ec17b572ea4af0c1
HyeonWoo <rmakxh@naver.com>
2022-12-07 11:25:36 +0900

    feat: Create 07/Calendar
    - 캘린더 기능구현 완료
    - 추후 리팩토링 필요

--- 07.Calendar&DatePicker/app.js
+++ 07.Calendar&DatePicker/app.js
@@ -2,3 +2,4 @@ import Picker from './js/picker.js';
 
 // const datePicker = document.createElement()
 Picker(document.querySelector('.date-picker'));
+Picker(document.querySelector('.date-picker2'));
--- 07.Calendar&DatePicker/js/calendar/calendar.js
+++ 07.Calendar&DatePicker/js/calendar/calendar.js
@@ -1,64 +1,4 @@
-const MONTH = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-const render = ($calendar, dateState, newDate) => {
-  const { date, nowDate, clickedIndex } = dateState;
-  const firstDayIndex = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
-  const crrLastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-  const preLastDate = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-  const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
-
-  console.log(Boolean(new Date(undefined)));
-
-  console.log(firstDayIndex);
-  console.log(crrLastDate);
-  console.log(preLastDate);
-  console.log(lastDay);
-
-  // prettier-ignore
-  $calendar.innerHTML = `
-    <nav class="calendar-nav">
-      <button><i class="fa-solid fas fa-caret-left"></i></button>
-      <div class="calendar-title">
-        <h2>${MONTH[date.getMonth()]}</h2>
-        <span>${date.getFullYear()}</span>
-      </div>
-      <button><i class="fa-solid fas fa-caret-right"></i></button>
-    </nav>
-    <ul class="calendar-grid">
-      ${['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-        .map(day => `<li class="day ft-sv">${day}</li>`).join('')}
-      ${Array(firstDayIndex + crrLastDate + 6 - lastDay).fill(0).map((_, index) => {
-          if (index < firstDayIndex) return `<li class="ft-sv" data-date='${index}'>${
-            index - firstDayIndex + preLastDate + 1
-          }</li>`;
-
-          if (index < firstDayIndex + crrLastDate) return `<li class="${index % 7 === 0 ? 'ft-red' : ''} ${
-            date.getFullYear() === nowDate.getFullYear() &&
-            date.getDate() === nowDate.getDate() &&
-            index - firstDayIndex + 1 === nowDate.getDate()
-              ? 'now'
-              : ''
-          } date" data-date='${index}'>${index - firstDayIndex + 1}</li>`;
-
-          return `<li class="ft-sv" data-date='${index}'>${
-            index - crrLastDate - firstDayIndex + 1
-          }</li>`;
-        }).join('')}
-    </ul>`;
-};
+import { setDateState, CalendarRender } from './state.js';
 
 const Calendar = ($calendar, $datePicker) => {
   const dateState = {
@@ -66,15 +6,12 @@ const Calendar = ($calendar, $datePicker) => {
     nowDate: new Date(),
   };
 
-  render($calendar, dateState);
-
   $calendar.addEventListener('click', e => {
-    if (!e.target.matches('.calendar-grid > li')) return;
+    if (!e.target.matches('.calendar-grid > li') || e.target.matches('.day')) return;
     // $calendar.classList.add('hidden');
-    const clickedYear = document.querySelector('.calendar-title').lastElementChild.textContent;
-    const clickedMonth = MONTH.indexOf(document.querySelector('.calendar-title').firstElementChild.textContent) + 1;
-    const clickedDate = e.target.dataset.index;
-
+    const clickedYear = e.target.dataset.date.split('-')[0];
+    const clickedMonth = e.target.dataset.date.split('-')[1];
+    const clickedDate = e.target.dataset.date.split('-')[2];
     const dateClick = new CustomEvent('date-click', {
       detail: {
         clickedYear,
@@ -82,8 +19,22 @@ const Calendar = ($calendar, $datePicker) => {
         clickedDate,
       },
     });
+
     $datePicker.dispatchEvent(dateClick);
   });
-};
 
+  $calendar.addEventListener('click', e => {
+    if (!e.target.closest('.arrow')) return;
+    setDateState(e.target, dateState);
+    CalendarRender($calendar, dateState);
+    // if (e.target.closest('.arrow.left')) {
+    //   dateState.date = new Date(dateState.date.getFullYear(), dateState.date.getMonth() - 1, 1);
+    // }
+    // if (e.target.closest('.arrow.right')) {
+    //   dateState.date = new Date(dateState.date.getFullYear(), dateState.date.getMonth() + 1, 1);
+    // }
+    // render($calendar, dateState);
+  });
+  return dateState;
+};
 export default Calendar;
